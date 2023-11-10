7d1feed82c600a88320dc6c8bd7ff78dd8500bc0
donggyun <donggyunkim12@gmail.com>
2023-11-10 23:54:56 +0900

    feat(OutputView): 주문 메뉴 출력 기능 구현

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -13,6 +13,15 @@ const OutputView = {
   printPreview(date) {
     Console.print(OUTPUT_MESSAGE.OUTPUT_EVENT_PREVIEW_MESSAGE(date));
   },
+
+  printMyOrderMenu(myOrders) {
+    Console.print(OUTPUT_MESSAGE.OUTPUT_ORDER_MENU_TITLE);
+    for (let i = 0; i < myOrders.length; i++) {
+      const name = myOrders[i].menuInfo.name;
+      const amount = myOrders[i].amount;
+      Console.print(OUTPUT_MESSAGE.OUTPUT_EACH_ORDER_MENU(name, amount));
+    }
+  },
 };
 
 export default OutputView;
