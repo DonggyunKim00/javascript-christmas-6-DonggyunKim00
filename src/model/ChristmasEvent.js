d8820599976a7bc8f5b092181285f03cfdafa5ba
donggyun <donggyunkim12@gmail.com>
2023-11-11 15:30:39 +0900

    feat(ChristmasEvent): weekdaysEvent, weekendsEvent 기능 구현

--- src/model/ChristmasEvent.js
+++ src/model/ChristmasEvent.js
@@ -1,3 +1,5 @@
+import { EVENTDAY } from '../db/data.js';
+
 class ChristmasEvent {
   #applyEventObj = {
     dday: 0,
@@ -10,6 +12,26 @@ class ChristmasEvent {
   ddayEvent(date) {
     if (date >= 1 && date <= 25)
       this.#applyEventObj.dday = 1000 + (date - 1) * 100;
+  }
+
+  weekdaysEvent(date, myOrderList) {
+    if (EVENTDAY.weekdaysEvent.some((day) => day === date)) {
+      let dessertCount = 0;
+      myOrderList.forEach((menu) => {
+        if (menu.menuInfo.category === '디저트') dessertCount++;
+      });
+      this.#applyEventObj.weekdays = 2023 * dessertCount;
+    }
+  }
+
+  weekendsEvent(date, myOrderList) {
+    if (EVENTDAY.weekendsEvent.some((day) => day === date)) {
+      let mainCount = 0;
+      myOrderList.forEach((menu) => {
+        if (menu.menuInfo.category === '메인') mainCount++;
+      });
+      this.#applyEventObj.weekends = 2023 * mainCount;
+    }
   }
 }
 
