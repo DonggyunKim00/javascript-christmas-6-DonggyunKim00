c79fd5c3e600957158a4ff6ee6971a61bf61328b
donggyun <donggyunkim12@gmail.com>
2023-11-11 20:40:11 +0900

    feat(OutputView): 혜택 내역 출력 기능 구현

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -38,6 +38,25 @@ const OutputView = {
       ? Console.print(OUTPUT_MESSAGE.OUTPUT_EACH_ORDER_MENU('샴페인', 1))
       : this.printNoneMatchEvent();
   },
+
+  printBenefitList(christmasEvent) {
+    Console.print(OUTPUT_MESSAGE.OUTPUT_BENEFIT_LIST_TITLE);
+    if (christmasEvent.isAllZero()) this.printNoneMatchEvent();
+    else this.printBenefitHistory(christmasEvent);
+  },
+
+  printBenefitHistory(christmasEvent) {
+    const myBenefitArr = christmasEvent.translateArr();
+    for (let i = 0; i < myBenefitArr.length; i++) {
+      if (myBenefitArr[i][1] !== 0) {
+        Console.print(
+          `${OUTPUT_MESSAGE.OUTPUT_BENEFIT_LIST(
+            myBenefitArr[i][0],
+          )}${OUTPUT_MESSAGE.OUTPUT_DISCOUNT_MONEY(myBenefitArr[i][1])}`,
+        );
+      }
+    }
+  },
 };
 
 export default OutputView;
