67a6c8d13175dde233c35cfd4c471960d953321f
donggyun <donggyunkim12@gmail.com>
2023-11-12 11:22:26 +0900

    feat(App): 도메인 로직 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import EventPlanner from './controller/EventPlanner.js';
+
 class App {
-  async run() {}
+  async run() {
+    const eventPlanner = new EventPlanner();
+
+    await eventPlanner.init();
+    await eventPlanner.previewResult();
+  }
 }
 
 export default App;
