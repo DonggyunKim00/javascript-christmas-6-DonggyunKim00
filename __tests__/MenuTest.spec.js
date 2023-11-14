598a5ed7ebef0900b86489a778c4af3ba673efe3
donggyun <donggyunkim12@gmail.com>
2023-11-14 19:11:18 +0900

    test(MenuTest): 입력받은 메뉴가 메뉴판에 없을때, 예외처리 테스트 구현

--- __tests__/MenuTest.spec.js
+++ __tests__/MenuTest.spec.js
@@ -1,3 +1,4 @@
+import { ERROR_MESSAGE } from '../src/constant/message';
 import Menu from '../src/model/Menu';
 
 describe('Menu 클래스 테스트', () => {
@@ -18,5 +19,33 @@ describe('Menu 클래스 테스트', () => {
 
     expect(menu.menuInfo).toEqual(expectedMenuInfo);
     expect(menu.amount).toEqual(expectedAmount);
+  });
+});
+
+describe('예외처리 테스트', () => {
+  test('입력받은 메뉴가 메뉴판에 없을때, 예외처리한다.', () => {
+    // give
+    const invalidInput = [
+      ['우테코파스타', 3],
+      ['해물찜', 7],
+      ['제로사이다', 1],
+    ];
+
+    const errors = invalidInput.map((input) => {
+      try {
+        // when
+        Menu.create(input[0], input[1]);
+        return true;
+      } catch (err) {
+        // then
+        return err;
+      }
+    });
+
+    expect(errors).toEqual([
+      ERROR_MESSAGE.INVALID_EXIST_MENU,
+      ERROR_MESSAGE.INVALID_EXIST_MENU,
+      ERROR_MESSAGE.INVALID_EXIST_MENU,
+    ]);
   });
 });
