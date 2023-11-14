24d89edc5b566d32bfd5dba73aee1d872dd31b96
donggyun <donggyunkim12@gmail.com>
2023-11-14 20:40:51 +0900

    test(MyOrderTest): 이벤트 적용 전 총 주문 금액 계산 테스트 구현

--- __tests__/MyOrderTest.spec.js
+++ __tests__/MyOrderTest.spec.js
@@ -2,7 +2,7 @@ import Menu from '../src/model/Menu.js';
 import MyOrder from '../src/model/MyOrder.js';
 
 describe('MyOrder 클래스 테스트', () => {
-  test('입력받은 메뉴의 정보와 갯수를 나의 주문목록에 담는다.', async () => {
+  test('입력받은 메뉴의 정보와 갯수를 나의 주문목록에 담는다.', () => {
     // given
     const menuListArr = [
       ['해산물파스타', 2],
@@ -23,5 +23,23 @@ describe('MyOrder 클래스 테스트', () => {
     ];
 
     expect(myOrders.getMyOrderList()).toEqual(expected);
+  });
+
+  test('이벤트 적용 전 총 주문 금액을 계산한다.', () => {
+    // given
+    const menuListArr = [
+      ['해산물파스타', 2],
+      ['바비큐립', 1],
+      ['레드와인', 1],
+      ['아이스크림', 3],
+    ];
+
+    // when
+    const myOrders = new MyOrder(menuListArr);
+
+    // then
+    const expected = 35000 * 2 + 54000 * 1 + 60000 * 1 + 5000 * 3;
+
+    expect(myOrders.getTotalMyOrderMoney()).toEqual(expected);
   });
 });
