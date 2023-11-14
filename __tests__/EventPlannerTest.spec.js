057ffb35a531bd660f5e440dd39254deb1cb594a
donggyun <donggyunkim12@gmail.com>
2023-11-14 13:55:42 +0900

    test(EventPlannerTest): 주문 형식 정상 입력일때, 주문 목록을 생성시도한다. 테스트 구현

--- __tests__/EventPlannerTest.spec.js
+++ __tests__/EventPlannerTest.spec.js
@@ -1,5 +1,6 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
 import EventPlanner from '../src/controller/EventPlanner.js';
+import MyOrder from '../src/model/MyOrder.js';
 
 const mockQuestion = (inputs) => {
   MissionUtils.Console.readLineAsync = jest.fn();
@@ -21,5 +22,22 @@ describe('날짜 입력', () => {
     const expected = 3;
 
     expect(date).toEqual(expected);
+  });
+
+  test('주문 형식 정상 입력일때, 주문 목록을 생성시도한다.', async () => {
+    // // given
+    mockQuestion('해산물파스타-1,레드와인-2,초코케이크-1');
+
+    // // when
+    const eventPlanner = new EventPlanner();
+    const myOrders = await eventPlanner.setOrder();
+
+    // // then
+    const menuListArr = eventPlanner.splitInputMenu(
+      '해산물파스타-1,레드와인-2,초코케이크-1',
+    );
+    const expected = new MyOrder(menuListArr);
+
+    expect(myOrders).toEqual(expected);
   });
 });
